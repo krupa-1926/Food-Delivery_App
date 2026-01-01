41b3ce6806e65d3477b57bc15445955000a671db
krupa-1926 <patelkrupa1926@gmail.com>
2026-01-01 21:13:05 +0530

    url added

--- frontend/src/components/context/StoreContext.jsx
+++ frontend/src/components/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const [search, setSearch] = useState("");
-  const url = "http://localhost:4000";
+  const url =  import.meta.env.VITE_API_URL;
   const [token, setToken] = useState("");
 
   //remove food_list state 
