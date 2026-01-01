6913105102f19ef7b68cd61fa1ac5d1e307668bc
krupa-1926 <patelkrupa1926@gmail.com>
2026-01-01 16:32:33 +0530

    update cart page

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,12 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://patelkrupa93281912_db_user:bNo6ZntmT2nEhGt6@learningcluster.cfuylsn.mongodb.net/"
+      "mongodb+srv://patelkrupa93281912_db_user:cItvfkX80U2SQUoW@fooddelivery.elzsgck.mongodb.net/?appName=FoodDeliver"
     )
     .then(() => {
       console.log("DB connected");
     });
 };
+// patelkrupa93281912_db_user
+// cItvfkX80U2SQUoW
+// mongodb+srv://patelkrupa93281912_db_user:cItvfkX80U2SQUoW@fooddelivery.elzsgck.mongodb.net/?appName=FoodDelivery
